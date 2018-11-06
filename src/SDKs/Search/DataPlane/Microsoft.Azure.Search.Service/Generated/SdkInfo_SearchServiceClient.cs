66f3a6bab0e3f18b06b30f744069b3bcfcb401e9
Nate Ko <nateko@microsoft.com>
2018-11-06 15:08:37 -0800

    [Azure Search] regenerate the skillsets API with the updated config (readme)

--- src/SDKs/Search/DataPlane/Microsoft.Azure.Search.Service/Generated/SdkInfo_SearchServiceClient.cs
+++ src/SDKs/Search/DataPlane/Microsoft.Azure.Search.Service/Generated/SdkInfo_SearchServiceClient.cs
@@ -28,5 +28,16 @@ namespace Microsoft.Azure.Search
               }.AsEnumerable();
           }
       }
+      // BEGIN: Code Generation Metadata Section
+      public static readonly String AutoRestVersion = "latest";
+      public static readonly String AutoRestBootStrapperVersion = "autorest@2.0.4283";
+      public static readonly String AutoRestCmdExecuted = "cmd.exe /c autorest.cmd E:\\last\\azure-rest-api-specs\\specification\\search\\data-plane\\Microsoft.Azure.Search.Service\\readme.md --csharp --version=latest --reflect-api-versions --csharp-sdks-folder=E:\\last\\azure-sdk-for-net\\src\\SDKs";
+      public static readonly String GithubForkName = "Azure";
+      public static readonly String GithubBranchName = "master";
+      public static readonly String GithubCommidId = "487d177c5fc08020c8c72ed11a4ab6e274927413";
+      public static readonly String CodeGenerationErrors = "";
+      public static readonly String GithubRepoName = "azure-rest-api-specs";
+      // END: Code Generation Metadata Section
   }
 }
+
